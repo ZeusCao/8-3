134f89e14301cd4e2fb0bc5c92055fba0711b7aa
曹檑 <1090491594@qq.com>
2017-08-03 14:12:53 +0800

    初始化

---
+++ 8-3-模仿新浪微博/Classes/View(视图和控制器)/Message/WBMessageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WBMessageViewController.swift
+//  8-3-模仿新浪微博
+//
+//  Created by Zeus on 2017/8/3.
+//  Copyright © 2017年 Zeus. All rights reserved.
+//
+
+import UIKit
+
+class WBMessageViewController: WBBaseController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
